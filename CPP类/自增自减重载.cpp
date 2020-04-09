8c5109d8ee83e381ede98cb3dfb97f0c0aa630fb
Ren996 <60252826+Ren996@users.noreply.github.com>
2020-04-09 14:04:41 +0800

    Update 自增自减重载.cpp

--- CPP类/自增自减重载.cpp
+++ CPP类/自增自减重载.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+//在相同情况下 前置自增自减的运算速度快于后置，
 class Demo
 {
 private:
@@ -8,10 +9,10 @@ public:
     Demo(int n):num(n)    //切记初始化列表，不然报错显示没有办法把  num  初始化
     {
 
-    } 
+    }
     Demo & operator++();    //前置自增
     Demo operator++(int);   //后置自增
-    operator int()
+    operator int()          //强制类型转换  将对象转换为int的整型
     {
         return num;
     }
@@ -47,10 +48,10 @@ Demo operator--(Demo &d,int k)
 int main()
 {
     Demo d(5);
-    cout << (d++)<< "   ";
-    cout << d << "   ";
-    cout << (++d) << endl;
-    cout << (d--)<< "   ";
-    cout << d << "   ";
-    cout << (--d) << endl;
+    cout << (int)(d++)<< "   ";         //可省略 (int)
+    cout << (int)d << "   ";            //可省略 (int)
+    cout << (int)(++d) << endl;         //可省略 (int)
+    cout << (int)(d--)<< "   ";         //可省略 (int)
+    cout << (int)d << "   ";            //可省略 (int)
+    cout << (int)(--d) << endl;         //可省略 (int)
 }
