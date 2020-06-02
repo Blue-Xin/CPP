0489ed23403d062491fa2b5aefde7db53c476339
Ren996 <60252826+Ren996@users.noreply.github.com>
2020-06-02 21:10:54 +0800

    Add files via upload

---
+++ STL/auto的用法.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<list>
+#include<set>
+#include<map>
+#include<stack>
+using namespace std;
+int main()
+{
+    vector<string> myvector;
+    myvector.push_back("张三");
+    myvector.push_back("李四");
+    for(auto v :myvector)
+    {
+        cout << v << endl;
+    }
+    cout << endl;
+    deque<string> mydeque;
+    mydeque.push_back("张三");
+    mydeque.push_back("李四");
+    for(auto d:mydeque)
+    {
+        cout << d <<endl;
+    }
+    cout << endl;
+    list<string> mylist;
+    mylist.push_back("张三");
+    mylist.push_back("李四");
+    for(auto l : mylist)
+    {
+        cout << l << endl;
+    }
+    cout << endl;
+    //set stack mutliset 和上面用法一致
+    //关于map 存在映射关系，所以用法稍有不同
+    map<string , int> mymap;
+    mymap.insert(pair<string , int>("张三",22));
+    mymap.insert(pair<string , int>("李四",22));
+    for(auto &m : mymap)
+    {
+        cout << m.first << " " << m.second << endl;
+    }
+}
